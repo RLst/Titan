c9e60c1b6ff29b52a15aae2ee73b451939610219
Tony Le <letony@icloud.com>
2019-02-23 15:36:22 +1100

    Conflicts fixed (again)... crappy platformer; camera follows, player can jump around and platform etc

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+namespace Titan
+{
+    public class CameraFollow : MonoBehaviour
+    {
+        Camera cam;
+
+        // [SerializeField] int minPanZone = 200; //The size of the zone where if the 
+        [SerializeField] Transform subject;
+
+        void Start()
+        {
+            cam = Camera.main;
+        }
+
+        void Update()
+        {
+            cam.transform.position = subject.transform.position;
+
+            //Move camera with the subject
+            //Horizontal
+            // int subjectAndZoneIntersection  = (int)cam.transform.position.x - cam.pixelWidth / 2;
+            // if (subjectAndZoneIntersection < minPanZone)
+            // {
+            // 	cam.transform.Translate()
+            // }
+        }
+    }
+}
