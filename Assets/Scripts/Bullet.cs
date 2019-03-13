17c72be99b16dcb9a749b14aa1a1b9f59f37c300
Tony Le <letony@icloud.com>
2019-03-14 03:25:45 +1100

    Basic bullet

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -2,11 +2,26 @@
 
 namespace Titan
 {
+	[RequireComponent(typeof(Rigidbody2D))]
     public class Bullet : MonoBehaviour
     {
 		[SerializeField] Vector2 trajectory;
 		[SerializeField] float speed;
 		[SerializeField] float mass;
+
+		new Rigidbody2D rigidbody;
+
+		void Awake()
+		{
+			rigidbody = GetComponent<Rigidbody2D>();
+		}
+
+		void Start()
+		{
+			rigidbody.velocity = trajectory * speed;
+			Destroy(this, 5f);	//Crap
+		}
+
     }
 
 }
