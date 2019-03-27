29bcc6d1afe5aba7eb78f813549b882815cec02c
Tony Le <letony@icloud.com>
2019-03-27 16:54:55 +1100

    Started implemention of turrets

---
+++ Assets/Scripts/Turret/HMGTurret.cs
@@ -0,0 +1,8 @@
+﻿namespace Titan
+{
+    public class HMGTurret : Turret
+    {
+
+
+    }
+}
--- Assets/Scripts/Turret/Turret.cs
+++ Assets/Scripts/Turret/Turret.cs
@@ -4,7 +4,12 @@ namespace Titan
 {
 	public class Turret : MonoBehaviour
 	{
+		//This class might not need to be subclassed and can handle all turrets
+
 		public float health;
+
+		[SerializeField] Transform ordnance;		//The actual gun component of this turret ie. HMG, grenade launcher, sniper, etc
+		
 	}
 
 }
